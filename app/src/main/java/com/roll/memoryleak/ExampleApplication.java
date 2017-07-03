3c052cbc314a3cd37de42e1e8d101a360c8314eb
Denys Romanchenko <6104172@gmail.com>
2017-07-03 23:28:23 +0300

    init

---
+++ app/src/main/java/com/roll/memoryleak/ExampleApplication.java
@@ -0,0 +1,17 @@
+package com.roll.memoryleak;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by RDL on 01/07/2017.
+ */
+
+public class ExampleApplication extends Application {
+
+       @Override public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
+    }
+}
---
+++ app/src/main/java/com/roll/memoryleak/MainActivity.java
@@ -0,0 +1,65 @@
+package com.roll.memoryleak;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private AsyncTask asyncTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textid);
+
+        asyncTask = new BackgroundTask(textView).execute();
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+//        asyncTask.cancel(true);
+        super.onDestroy();
+
+    }
+
+    private class BackgroundTask extends AsyncTask<Void, Void, String> {
+
+        private final WeakReference<TextView> textViewWeakReference;
+
+        private BackgroundTask(TextView textView) {
+            this.textViewWeakReference = new WeakReference<>(textView);
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                Thread.sleep(10000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "some string";
+        }
+
+        @Override
+        protected void onCancelled() {
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            TextView view = textViewWeakReference.get();
+            if (view != null)
+                view.setText(s);
+        }
+    }
+
+}
